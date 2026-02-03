13390a4998efc1e0978eaa0ced44e9f3d97b5105
Ethan <ehowe1215@gmail.com>
2026-02-02 19:30:29 -0500

    Updated scripts in engine
    Copied E Handler's uploaded scripts from git web to the Unity project:
    Player, Enemy, Pellet, and Teleporter.

---
+++ Assets/Scripts/Pellet.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Pellet : MonoBehaviour
+{
+    //get Player
+    private Player player;
+    //get Collider2D
+    private Collider2D col;
+    //get Rigidbody2D
+    private Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = FindFirstObjectByType<Player>();
+        col = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // If the player enters the trigger, destroy the pellet
+        if (col.IsTouching(player.GetComponent<Collider2D>()))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
