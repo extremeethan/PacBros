5c3ed7b6791f841eed45a97a78aee93aa0b11267
Thomas B. <111815848+moonshinetcb@users.noreply.github.com>
2026-02-10 17:06:51 -0500

    added menu sound

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,17 +3,41 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    // Audio
+    public AudioSource audioSource;
+    public AudioClip menuButtonSound;
 
     public void Playgame()
     {
+        // play button sound
+        if (audioSource != null && menuButtonSound != null)
+        {
+            audioSource.PlayOneShot(menuButtonSound);
+        }
+
         SceneManager.LoadSceneAsync("Level_01");
     }
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
+
     public void Credits()
     {
+        // play button sound
+        if (audioSource != null && menuButtonSound != null)
+        {
+            audioSource.PlayOneShot(menuButtonSound);
+        }
+
         SceneManager.LoadSceneAsync("Credits");
     }
+
+    public void QuitGame()
+    {
+        // play button sound
+        if (audioSource != null && menuButtonSound != null)
+        {
+            audioSource.PlayOneShot(menuButtonSound);
+        }
+
+        Application.Quit();
+    }
 }
+
